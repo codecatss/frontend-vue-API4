15de555c196e2897b028d82bd84f1d2e272f0be6
danko caboski <caboskidanko@gmail.com>
2024-03-10 21:47:32 -0300

    feat: template base

---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+    <header>
+
+        <img src="../assets/oracle.svg" alt="Oracle Logo" />
+
+        
+        <nav>
+            <ul>
+                <RouterLink to="/auth">Login</RouterLink>
+                <RouterLink to="/">Home</RouterLink>
+            </ul>
+        </nav>
+
+    </header>
+</template>
+
+    <script>
+    import { RouterLink, RouterView } from 'vue-router';
+
+    export default {
+        name: 'HeaderComponent',
+    };
+    </script>
+
+
+<style>
+header {
+    width: 100%;
+    background-color: var(--oracles-header-gray);
+    padding: 10px;
+    
+    line-height: 1.5;
+    position: fixed;
+    top: 0;
+    margin-right: 0%;
+}
+
+nav {
+    display: flex;
+    justify-content: right;
+    width: 100%;
+    text-align: center;
+    padding: 1vh;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  font-size: 1vw;
+  font-weight: 700;
+  color: var(--vt-c-white-mute);
+}
+
+@media (max-width: 1023px) {
+
+    nav a {
+        font-size: 3vw;
+        padding: 0,5vh;
+    }
+
+}
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" id="email" v-model="email" required>
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" id="password" v-model="password" required>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+
+    <RouterView />
+</template>
+
+<script>
+import { RouterLink, RouterView } from 'vue-router';
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        login() {
+            // implemente a lógica de login aqui
+        }
+    }
+}
+</script>
