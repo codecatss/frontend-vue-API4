9b049a73c4eb7d09ee36a7ebd2d2bbe55510fa09
Danko Caboski <caboskidanko@gmail.com>
2024-04-09 20:16:09 -0300

    feat: base component import

--- src/components/import.vue
+++ src/components/import.vue
@@ -1,40 +1,37 @@
 <template>
-    <input class="input" type="text" name="name" placeholder="File name" v-model="fileName" required>
-    <input class="file-input" type="file" ref="file" name="file">
+   <form  @submit.prevent="submitForm">
+      <input type="file" @change="onFileChange">
+    <button @click="submitForm">Submit</button>
+   </form>
   </template>
   
   <script>
-  // eslint-disable-next-line regex/invalid
   import axios from 'axios'
   
   export default {
     name: 'importComp',
 
-    // eslint-disable-next-line vue/component-api-style
     data() {
       return {
-        fileName: '',
-        attachment: null,
-        formData: null,
+        file: null
       }
     },
-    // eslint-disable-next-line vue/component-api-style
+
     methods: {
-      submitForm() {
-        this.formData = new FormData()
-        this.formData.append('name', this.fileName)
-        this.formData.append('attachment', this.attachment)
-  
-        axios.post('files/add', this.formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        }).then(response => {
-          console.log(response.data)
-        }).catch(error => {
-          console.log(error)
-        })
-      },
+      onFileChange(e) {
+      this.file = e.target.files[0];
+    },
+
+    submitForm() {
+      const formData = new FormData();
+      formData.append("file", this.file);
+      axios.post("http://localhost:8080/api/import-csv", formData)
+         .then(function (result) {
+            console.log(result);
+         }, function (error) {
+            console.log(error);
+         });
+      }
     },
   }
   </script>
