ea2017af6021d05d3d149f015876ba4facdfc45a
Larissa Fernanda <laliffr@gmail.com>
2024-04-21 16:04:28 -0300

    CATS-49 feat(AnalyticsSalesByStates.vue): Adiciona dados do banco e ordena lista de estados por número de parceiros

--- src/views/membros/AnalyticsSalesByStates.vue
+++ src/views/membros/AnalyticsSalesByStates.vue
@@ -1,168 +1,212 @@
 <script setup>
-const salesByStates = [
-  {
-    abbr: 'AC',
-    state: 'Acre',
-    sales: '0',
-    color: 'success',
-  },
-  {
-    abbr: 'AL',
-    state: 'Alagoas',
-    sales: '0',
-    color: 'error',
-  },
-  {
-    abbr: 'AP',
-    state: 'Amapá',
-    sales: '0',
-    color: 'warning',
-  },
-  {
-    abbr: 'AM',
-    state: 'Amazonas',
-    sales: '0',
-    color: 'secondary',
-  },
-  {
-    abbr: 'BA',
-    state: 'Bahia',
-    sales: '0',
-    color: 'error',
-  },
-  {
-    abbr: 'CE',
-    state: 'Ceará',
-    sales: '0',
-    color: 'success',
-  },
-  {
-    abbr: 'DF',
-    state: 'Distrito Federal',
-    sales: '0',
-    color: 'error',
-  },
-  {
-    abbr: 'ES',
-    state: 'Espírito Santo',
-    sales: '0',
-    color: 'warning',
-  },
-  {
-    abbr: 'GO',
-    state: 'Goiás',
-    sales: '0',
-    color: 'secondary',
-  },
-  {
-    abbr: 'MA',
-    state: 'Maranhão',
-    sales: '0',
-    color: 'error',
-  },
-  {
-    abbr: 'MT',
-    state: 'Mato Grosso',
-    sales: '0',
-    color: 'success',
-  },
-  {
-    abbr: 'MS',
-    state: 'Mato Grosso do Sul',
-    sales: '0',
-    color: 'error',
-  },
-  {
-    abbr: 'MG',
-    state: 'Minas Gerais',
-    sales: '0',
-    color: 'warning',
-  },
-  {
-    abbr: 'PA',
-    state: 'Pará',
-    sales: '0',
-    color: 'secondary',
-  },
-  {
-    abbr: 'PB',
-    state: 'Paraíba',
-    sales: '0',
-    color: 'error',
-  },
-  {
-    abbr: 'PR',
-    state: 'Paraná',
-    sales: '0',
-    color: 'success',
-  },
-  {
-    abbr: 'PE',
-    state: 'Pernambuco',
-    sales: '0',
-    color: 'error',
-  },
-  {
-    abbr: 'PI',
-    state: 'Piauí',
-    sales: '0',
-    color: 'warning',
-  },
-  {
-    abbr: 'RJ',
-    state: 'Rio de Janeiro',
-    sales: '0',
-    color: 'secondary',
-  },
-  {
-    abbr: 'RN',
-    state: 'Rio Grande do Norte',
-    sales: '0',
-    color: 'error',
-  },
-  {
-    abbr: 'RS',
-    state: 'Rio Grande do Sul',
-    sales: '0',
-    color: 'success',
-  },
-  {
-    abbr: 'RO',
-    state: 'Rondônia',
-    sales: '0',
-    color: 'error',
-  },
-  {
-    abbr: 'RR',
-    state: 'Roraima',
-    sales: '0',
-    color: 'warning',
-  },
-  {
-    abbr: 'SC',
-    state: 'Santa Catarina',
-    sales: '0',
-    color: 'secondary',
-  },
-  {
-    abbr: 'SP',
-    state: 'São Paulo',
-    sales: '0',
-    color: 'error',
-  },
-  {
-    abbr: 'SE',
-    state: 'Sergipe',
-    sales: '0',
-    color: 'success',
-  },
-  {
-    abbr: 'TO',
-    state: 'Tocantins',
-    sales: '0',
-    color: 'error',
-  },
-];
+import { ref } from 'vue';
+import { api } from '@/service/apiConfig.js';
+
+
+function getListSalesByStates(){
+  let salesByStates = [
+    {
+      abbr: 'AC',
+      state: 'Acre',
+      sales: '0',
+      color: 'success',
+    },
+    {
+      abbr: 'AL',
+      state: 'Alagoas',
+      sales: '0',
+      color: 'error',
+    },
+    {
+      abbr: 'AP',
+      state: 'Amapá',
+      sales: '0',
+      color: 'warning',
+    },
+    {
+      abbr: 'AM',
+      state: 'Amazonas',
+      sales: '0',
+      color: 'secondary',
+    },
+    {
+      abbr: 'BA',
+      state: 'Bahia',
+      sales: '0',
+      color: 'error',
+    },
+    {
+      abbr: 'CE',
+      state: 'Ceará',
+      sales: '0',
+      color: 'success',
+    },
+    {
+      abbr: 'DF',
+      state: 'Distrito Federal',
+      sales: '0',
+      color: 'error',
+    },
+    {
+      abbr: 'ES',
+      state: 'Espírito Santo',
+      sales: '0',
+      color: 'warning',
+    },
+    {
+      abbr: 'GO',
+      state: 'Goiás',
+      sales: '0',
+      color: 'secondary',
+    },
+    {
+      abbr: 'MA',
+      state: 'Maranhão',
+      sales: '0',
+      color: 'error',
+    },
+    {
+      abbr: 'MT',
+      state: 'Mato Grosso',
+      sales: '0',
+      color: 'success',
+    },
+    {
+      abbr: 'MS',
+      state: 'Mato Grosso do Sul',
+      sales: '0',
+      color: 'error',
+    },
+    {
+      abbr: 'MG',
+      state: 'Minas Gerais',
+      sales: '0',
+      color: 'warning',
+    },
+    {
+      abbr: 'PA',
+      state: 'Pará',
+      sales: '0',
+      color: 'secondary',
+    },
+    {
+      abbr: 'PB',
+      state: 'Paraíba',
+      sales: '0',
+      color: 'error',
+    },
+    {
+      abbr: 'PR',
+      state: 'Paraná',
+      sales: '0',
+      color: 'success',
+    },
+    {
+      abbr: 'PE',
+      state: 'Pernambuco',
+      sales: '0',
+      color: 'error',
+    },
+    {
+      abbr: 'PI',
+      state: 'Piauí',
+      sales: '0',
+      color: 'warning',
+    },
+    {
+      abbr: 'RJ',
+      state: 'Rio de Janeiro',
+      sales: '0',
+      color: 'secondary',
+    },
+    {
+      abbr: 'RN',
+      state: 'Rio Grande do Norte',
+      sales: '0',
+      color: 'error',
+    },
+    {
+      abbr: 'RS',
+      state: 'Rio Grande do Sul',
+      sales: '0',
+      color: 'success',
+    },
+    {
+      abbr: 'RO',
+      state: 'Rondônia',
+      sales: '0',
+      color: 'error',
+    },
+    {
+      abbr: 'RR',
+      state: 'Roraima',
+      sales: '0',
+      color: 'warning',
+    },
+    {
+      abbr: 'SC',
+      state: 'Santa Catarina',
+      sales: '0',
+      color: 'secondary',
+    },
+    {
+      abbr: 'SP',
+      state: 'São Paulo',
+      sales: '0',
+      color: 'error',
+    },
+    {
+      abbr: 'SE',
+      state: 'Sergipe',
+      sales: '0',
+      color: 'success',
+    },
+    {
+      abbr: 'TO',
+      state: 'Tocantins',
+      sales: '0',
+      color: 'error',
+    },
+  ];
+
+  return salesByStates;
+};
+
+async function putDataInSalesByStates(){
+  async function fetchData() {
+    const response = await api.get("/dash/state-per-company")
+    return response.data;
+  };
+
+  const data = await fetchData();
+  let salesByStates = getListSalesByStates();
+
+  data.forEach((item) => {
+    try{
+      item.state = decodeURIComponent(escape(item.state));
+    } catch (e) {
+      // do nothing;
+    }
+    let state = salesByStates.find((state) => state.state === item.state);
+    if (state) {
+      state.sales = item.companyCount;
+    };
+  });
+
+  return salesByStates;
+}
+
+async function orderSalesByStates(){
+  let salesByStates = await putDataInSalesByStates();
+  salesByStates.sort((a, b) => Number(b.sales) - Number(a.sales));
+  return salesByStates;
+}
+
+let salesByStates = ref([]);
+
+onMounted(async () => {
+  salesByStates.value = await orderSalesByStates();
+});
 
 </script>
 
