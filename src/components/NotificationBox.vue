4bdd450c8cf23d447ce1f47540ccc495d0e1fb6b
alec <alecrondel@hotmail.com>
2024-05-02 13:56:06 -0300

    :feat: implementando barra de rolagem

--- src/components/NotificationBox.vue
+++ src/components/NotificationBox.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-card class="notificacaoBox" :variant="elevated">
+  <v-card class="notificacaoBox">
     <div class="notificacao" v-if="notificacoes.length > 0">
-      <div v-for="(notif, index) in notificacoes" :key="index" class="cardDiv" style="width: 90%;">
+      <div v-for="(notif, index) in notificacoes" :key="index" class="cardDiv">
         <v-card-title class="notificacaoTitle pt-2 pb-0">
           {{ notif.company }}
         </v-card-title>
@@ -12,11 +12,11 @@
           <p v-if="notif.expertise.length < 51">
             {{ notif.expertise }}
           </p>
-          <p v-else class="expertiseBig">
+          <p v-else class="expertiseBig pb-0">
             {{ notif.expertise }}
           </p>
         </v-card-text>
-        <v-card-text class="notificacaoText pt-1 pb-2">
+        <v-card-text class="notificacaoText pt-0 pb-2">
           vencimento em <p class="timeLeft">{{ notif.timeLeft }} dias</p>.
         </v-card-text>
         <v-card-text class="notificacaoText">
@@ -147,8 +147,10 @@ onMounted(async () => {
 <style scoped>
   .notificacaoBox {
     width: 350%;
+    max-height: 40vmax;
+    overflow-y: auto;
     text-align: left;
-    box-shadow: 5px 5px 15px rgba(0,0,0,0.25);
+    box-shadow: 0px 5px 15px 3px rgba(0,0,0,0.25);
   }
 
   .notificacaoTitle {
